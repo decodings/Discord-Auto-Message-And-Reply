e17f8a6239d08cbe7247b800def08d771ba92e68
decodings <109295864+decodings@users.noreply.github.com>
2022-12-04 07:50:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import discord, asyncio
 
 token = '' # https://www.youtube.com/watch?v=YEgFvgg7ZPI
-reply_message = 'Join discord.gg/ to buy limiteds!' # Message to reply with when somebody DMs the token.
-main_message = 'DM if you want to buy limiteds.' # Message to send in the channel.
-channel_id = 0000000000000000000 # Channel ID to send the message in.
-delay = 300 # In seconds.
+replyMessage = 'Join discord.gg/ to buy limiteds!' # Message to reply with when somebody DMs the token.
+mainMessage = 'DM if you want to buy limiteds.' # Message to send in the channel.
+channelId = 0000000000000000000 # Channel ID to send the messages in.
+delay = 300 # Delay between each message in seconds.
 
 class Main(discord.Client):
     async def on_ready(self):
         print('Logged in as %s.' % self.user)
         while True:
-            channel = self.get_channel(channel_id)
-            await channel.send(main_message)
+            channel = self.get_channel(channelId)
+            await channel.send(mainMessage)
             print('Sent message in #%s.' % channel.name)
             await asyncio.sleep(delay)
 
@@ -20,7 +20,7 @@ class Main(discord.Client):
             if message.author.id != self.user.id:
                 with open('blacklist.txt', 'r', encoding = 'UTF-8') as file:
                     if str(message.author.id) not in file.read():
-                        await message.reply(reply_message)
+                        await message.reply(replyMessage)
                         print('Replied to %s.' % message.author.name)
                         with open('blacklist.txt', 'a', encoding = 'UTF-8') as file:
                             file.write('%s\n' % message.author.id)
